2f7f4d5637e4252da966f38c3322e0a73207aa7a
Julian <yizman@list.ru>
2024-03-08 22:08:08 +0500

    test/auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,8 +10,8 @@ import cors from "cors";
 const app = express();
 const port = config.port;
 
+app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
-app.use(cors());
 
 app.use("/auth", authController);
 app.use("/user", usersController);
