b10bae625aa5c5fa165ccf31d3c8c6e5b16e8b86
Rustam G. <gadzamatovrus@mail.ru>
2024-03-08 10:23:05 +0500

    add: register panel

---
+++ Our-Project/src/components/auth_panels/auth_register.svelte
@@ -0,0 +1,28 @@
+<script>
+    let input_name = "";
+    let input_email = "";
+    let input_password = "";
+</script>
+
+<div class="register_panel">
+    <h2>Register</h2>
+    <input class="input_text" type="text" name="name" bind:value={input_name}/>
+    <input class="input_text" type="text" name="email" bind:value={input_email}/>
+    <input class="input_text" type="password" name="password" bind:value={input_password}/>
+    <button class="input_btn">Log in</button>
+</div>
+
+<style>
+    .register_panel {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        gap: 6px;
+    }
+    .input_text {
+        width: 30%;   
+    }
+    .input_btn {
+        width: fit-content;
+    }
+</style>
--- Our-Project/src/routes/auth/+page.svelte
+++ Our-Project/src/routes/auth/+page.svelte
@@ -1,6 +1,16 @@
 <script>
     import LoginPanel from "$comp/auth_panels/auth_login.svelte";
+    import RegisterPanel from "$comp/auth_panels/auth_register.svelte";
 </script>
 
 <h1>Auth page</h1>
-<LoginPanel/>
+<div class="panels">
+    <LoginPanel/>
+    <RegisterPanel/>
+</div>
+
+<style>
+    .panels {
+        display: flex;
+    }
+</style>
