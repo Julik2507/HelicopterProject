a2c7d08eb41e9f060073804cce0db5453e8bd0a5
Rustam G. <gadzamatovrus@mail.ru>
2024-03-08 10:16:26 +0500

    Created layout

--- Our-Project/src/components/auth_panels/auth_login.svelte
+++ Our-Project/src/components/auth_panels/auth_login.svelte
@@ -1,1 +1,26 @@
-<h3>Login</h3>
+<script>
+    let input_email = "";
+    let input_password = "";
+</script>
+
+<div class="login_panel">
+    <h2>Login</h2>
+    <input class="input_text" type="text" name="email" bind:value={input_email}/>
+    <input class="input_text" type="password" name="password" bind:value={input_password}/>
+    <button class="input_btn">Log in</button>
+</div>
+
+<style>
+    .login_panel {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        gap: 6px;
+    }
+    .input_text {
+        width: 30%;   
+    }
+    .input_btn {
+        width: fit-content;
+    }
+</style>
