5a95c988582b6efaeac4e85a73482f5b46bc6951
Rustam G. <gadzamatovrus@mail.ru>
2024-03-08 21:33:20 +0500

    Add: Prime button

---
+++ Our-Project/src/components/ui_kit/prime_btn.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let text = "";
+</script>
+
+<button class="prime_btn"><p class="btn_text">{text}</p></button>
+
+<style>
+    .prime_btn {
+        background-color: #F23B0D;
+        border-color: #F23B0D;
+        border-radius: 10px;
+        width: fit-content;
+        padding: 0px 16px;
+    }
+    .btn_text {
+        font-family: Epilogue;
+        font-size: 14px;
+        font-weight: 700;
+        line-height: 21px;
+        letter-spacing: 0.20999999344348907px;
+        text-align: left;
+        color: #FFFFFF;
+    }
+</style>
--- Our-Project/src/routes/+page.svelte
+++ Our-Project/src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import PrimeBtn from "$comp/ui_kit/prime_btn.svelte";
+</script>
 
-<a href="/auth">Auth link</a>
+<PrimeBtn text="Сообщение"/>
