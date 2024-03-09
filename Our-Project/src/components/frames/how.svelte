fca68e1b36f9b6eddb6e3fc64060af3a18eb8d1c
Rustam G. <gadzamatovrus@mail.ru>
2024-03-09 10:54:31 +0500

    Update: how frame

--- Our-Project/src/components/frames/how.svelte
+++ Our-Project/src/components/frames/how.svelte
@@ -1,11 +1,17 @@
 <script>
-
+    import Hint from "$comp/ui_kit/hint.svelte";
 </script>
 
 <p class="how__title">Как это работает?</p>
+<div class="how__content">
+    <Hint source="src/img/Hint1.png" text="Выбрать"/>
+    <img class="how__arrow" src="src/img/Arrow.png" alt=""/>
+    <Hint source="src/img/Hint2.png" text="Оплатить"/>
+    <img class="how__arrow" src="src/img/Arrow.png" alt=""/>
+    <Hint source="src/img/Hint3.png" text="Наслаждаться"/>
+</div>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap');
     .how__title {
         font-family: Epilogue, sans-serif;
         font-size: 22px;
@@ -14,5 +20,13 @@
         letter-spacing: -0.33000001311302185px;
         text-align: left;
         color: #1C0F0D;
+    }
+    .how__content {
+        display: flex;
+        justify-content: space-between;
+    }
+    .how__arrow {
+        height: 190px;
+        margin-top: 40px;
     }
 </style>
