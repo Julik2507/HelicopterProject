2910c9d56f21a87656b7b544a40102c2b4eba93b
Rustam G. <gadzamatovrus@mail.ru>
2024-03-09 10:48:28 +0500

    Add: hint

---
+++ Our-Project/src/components/ui_kit/hint.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let source = "";
+    export let text = "";
+</script>
+
+<div class="hint__content">
+    <img class="hint__img" src={source} alt="">
+    <p class="hint__text">{text}</p>
+</div>
+
+<style>
+    .hint__content {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        align-items: center;
+    }
+    .hint__img {
+        width: 270px;
+    }
+    .hint__text {
+        padding: 0;
+    }
+</style>
