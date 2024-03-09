552af4666cac3a4b1f164fe636aacb7780bcb5fe
Julian <yizman@list.ru>
2024-03-09 17:20:37 +0500

    added gitignore

--- Our-Project/src/lib/index.ts
+++ Our-Project/src/lib/index.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
-import type { registerDTO } from "./dto";
+import type { loginDTO, registerDTO } from "./dto";
 
 export async function registerUser(dto: registerDTO) {
-  const result = await axios.post("http://localhost:3000/user/register", dto);
+  //validate//
+  const result = await axios.post("http://localhost:3000/auth/register", dto);
+  return result;
+}
+
+export async function loginUser(dto: loginDTO) {
+  //validate//
+  const result = await axios.post("http://localhost:3000/auth/login", dto);
   return result;
 }
