8a9be5981be303d9770b3f54aba7b48a1b056f7a
Rustam G. <gadzamatovrus@mail.ru>
2024-03-09 10:38:55 +0500

    Add: How frame

---
+++ Our-Project/src/components/frames/how.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<p class="how__title">Как это работает?</p>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap');
+    .how__title {
+        font-family: Epilogue, sans-serif;
+        font-size: 22px;
+        font-weight: 700;
+        line-height: 28px;
+        letter-spacing: -0.33000001311302185px;
+        text-align: left;
+        color: #1C0F0D;
+    }
+</style>
--- Our-Project/src/routes/+page.svelte
+++ Our-Project/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Header from "$comp/frames/header.svelte";
     import Attract from "$comp/frames/attract.svelte";
+    import How from "$comp/frames/how.svelte";
 </script>
 
 <Header/>
 <Attract/>
+<How/>
