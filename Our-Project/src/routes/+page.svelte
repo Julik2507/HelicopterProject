dc41175bae8dd824b4d0529a3e631cb3f7625392
Rustam G. <gadzamatovrus@mail.ru>
2024-03-14 12:12:18 +0500

    Add footer

--- Our-Project/src/routes/+page.svelte
+++ Our-Project/src/routes/+page.svelte
@@ -2,8 +2,10 @@
     import Header from "$comp/frames/header.svelte";
     import Attract from "$comp/frames/attract.svelte";
     import How from "$comp/frames/how.svelte";
+    import Footer from "$comp/frames/footer.svelte";
 </script>
 
 <Header/>
 <Attract/>
 <How/>
+<Footer/>
