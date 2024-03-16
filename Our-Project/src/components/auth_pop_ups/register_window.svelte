99a885ddddf2c21cd4dbe08a1c73bc1d1b32055a
Rustam G. <gadzamatovrus@mail.ru>
2024-03-16 10:33:47 +0500

    Add: simple modal

---
+++ Our-Project/src/components/auth_pop_ups/register_window.svelte
@@ -0,0 +1,42 @@
+<!-- src/Modal.svelte -->
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    export let isModalOpen = false;
+
+    const dispatch = createEventDispatcher();
+
+    function closeModal() {
+        isModalOpen = false;
+        dispatch('closeModal', { isModalOpen });
+    }
+</script>
+
+<div id="background" style="--display: {isModalOpen ? 'block' : 'none'};"></div>
+<div id="modal" style="--display: {isModalOpen ? 'block' : 'none'};">
+    <p>This is a modal window.</p>
+    <button on:click={closeModal}>Close Modal</button>
+</div>
+
+<style>
+    #background {
+        display: var(--display);
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    #modal {
+        display: var(--display);
+        position: fixed;
+        z-index: 2;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: #fff;
+        filter: drop-shadow(0 0 20px #333);
+    }
+</style>
--- Our-Project/src/routes/+page.svelte
+++ Our-Project/src/routes/+page.svelte
@@ -3,7 +3,21 @@
     import Attract from "$comp/frames/attract.svelte";
     import How from "$comp/frames/how.svelte";
     import Footer from "$comp/frames/footer.svelte";
+    import RegisterWindow from "$comp/auth_pop_ups/register_window.svelte";
+
+    let isModalOpen = false;
+
+    function openModal() {
+        isModalOpen = true;
+    }
+
+    function closeModal() {
+        isModalOpen = false;
+    }
 </script>
+
+<button id="testBtn" on:click={openModal}>Shiny button</button>
+<RegisterWindow isModalOpen={isModalOpen} on:closeModal={closeModal}/>
 
 <Header/>
 <Attract/>
