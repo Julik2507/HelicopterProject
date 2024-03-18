a284b3c04e9852b6f4e65b368ac87d3dcb4b9e33
Rustam G. <gadzamatovrus@mail.ru>
2024-03-18 17:21:23 +0500

    Add text_input

---
+++ Our-Project/src/components/ui_kit/text_input.svelte
@@ -0,0 +1,22 @@
+<script>
+    let inputValue = "";
+    export let placeholder = "";
+</script>
+
+<input class="text_input" type="text" bind:value={inputValue} {placeholder}/>
+
+<style>
+    .text_input {
+        width: 610px;
+        height: 63px;
+        top: 202px;
+        left: 36px;
+        border-radius: 21px;
+        border: 0;
+        padding-left: 20px;
+        background-color: #F5E8E8;
+        color: #967878;
+        font-size: 20px;
+        font-weight: bold;
+    }
+</style>
