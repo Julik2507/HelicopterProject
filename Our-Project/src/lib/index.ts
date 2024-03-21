a7bcdb513364796015ad4c4bdcd2574769dcde7a
Julian <yizman@list.ru>
2024-03-21 22:59:28 +0500

    add: upd catch errors in axios

--- Our-Project/src/lib/index.ts
+++ Our-Project/src/lib/index.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 import type { loginDTO, registerDTO } from "./dto";
 
 export async function registerUser(dto: registerDTO) {
-  const result = await axios.post("http://localhost:3000/auth/register", dto);
-
-  return result;
+  try {
+    const result = await axios.post("http://localhost:3000/auth/register", dto);
+    return result;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
 }
 
 export async function loginUser(dto: loginDTO) {
   try {
     const result = await axios.post("http://localhost:3000/auth/login", dto);
-    console.log("axios");
     console.log(result);
     return result.data;
   } catch (error: any) {
