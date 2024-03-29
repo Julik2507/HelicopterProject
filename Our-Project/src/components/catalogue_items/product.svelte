bbfbe2431a5c28d20bc3c8278784a18cf0693abb
Rustam G. <gadzamatovrus@mail.ru>
2024-03-29 22:43:17 +0500

    Add: default product

---
+++ Our-Project/src/components/catalogue_items/product.svelte
@@ -0,0 +1,57 @@
+<script>
+    export let type = "default";
+    export let img = "src/img/default_item.png";
+    export let name = "Багет";
+    export let amount = "180 г";
+    export let price = 85;
+</script>
+
+<div class="item">
+    <img src={img} alt={name}/>
+    <p class="item__name">{name}</p>
+    <p class="item__amount">{amount}</p>
+    <button class="item__button">{price} ₽ +</button>
+</div>
+
+<style>
+    .item {
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        width: fit-content;
+        height: fit-content;
+        gap: 0;
+        padding: 10px;
+    }
+    .item__name {
+        font-family: Epilogue, sans-serif;
+        font-size: 16px;
+        font-weight: 300;
+        line-height: 21px;
+        letter-spacing: 0.21px;
+        text-align: left;
+        margin: 0;
+    }
+    .item__amount {
+        font-family: Epilogue, sans-serif;
+        font-size: 12px;
+        font-weight: 700;
+        line-height: 21px;
+        letter-spacing: 0.21px;
+        text-align: left;
+        margin: 0;
+    }
+    .item__button {
+        width: fit-content;
+        background: #F1F1F1;
+        border-radius: 50px;
+        border: none;
+        font-family: Epilogue, sans-serif;
+        font-size: 13px;
+        font-weight: 400;
+        line-height: 21px;
+        letter-spacing: 0.21px;
+        text-align: left;
+        color: #FF335F;
+    }
+</style>
--- Our-Project/src/routes/catalogue/+page.svelte
+++ Our-Project/src/routes/catalogue/+page.svelte
@@ -1,16 +1,36 @@
 <script>
     import Header from "$comp/frames/header.svelte";
     import Footer from "$comp/frames/footer.svelte";
+    import Product from "$comp/catalogue_items/product.svelte";
 </script>
 
 <Header type="catalogue"/>
 <div class="catalogue__container">
-
+    <div class="catalogue__categories"></div>
+    <div class="catalogue__items">
+        <Product/>
+    </div>
 </div>
 <Footer/>
 
 <style>
     .catalogue__container {
+        display: flex;
         background-color: #F2F2F2;
+        padding: 25px;
+        gap: 10px;
+    }
+    .catalogue__categories {
+        background-color: white;
+        width: 490px;
+        height: 700px;
+        border-radius: 50px;
+    }
+    .catalogue__items {
+        background-color: white;
+        width: 1400px;
+        height: 650px;
+        border-radius: 50px;
+        padding: 25px;
     }
 </style>
