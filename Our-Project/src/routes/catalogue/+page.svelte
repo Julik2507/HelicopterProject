2c881dd9271bf3cf232f14e3bc7f2687f336c3ba
Rustam G. <gadzamatovrus@mail.ru>
2024-03-29 21:12:27 +0500

    Add: catalogue page

---
+++ Our-Project/src/routes/catalogue/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from "$comp/frames/header.svelte";
+    import Footer from "$comp/frames/footer.svelte";
+</script>
+
+<Header type="catalogue"/>
+<div class="catalogue__container">
+
+</div>
+<Footer/>
+
+<style>
+    .catalogue__container {
+        background-color: #F2F2F2;
+    }
+</style>
