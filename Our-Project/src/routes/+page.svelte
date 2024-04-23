385849a4350192643d60f072c8d29dfc9aae9d89
Julian <yizman@list.ru>
2024-04-23 23:49:24 +0500

    upd images

--- Our-Project/src/routes/+page.svelte
+++ Our-Project/src/routes/+page.svelte
@@ -5,6 +5,7 @@
     import Footer from "$comp/frames/footer.svelte";
 </script>
 
+<img src="http://localhost:3000/api/26ff9054-46a8-44f7-91a8-8ed1b8888c09.jpg" alt="" class="src">
 <Header type="main"/>
 <Attract/>
 <How/>
--- backend/build/modules/device/device.service.js
+++
@@ -1,6 +0,0 @@
-import { db } from "../../db/migrate.js";
-import { goods } from "../../db/schema.js";
-export async function createGoods(dto) {
-    return await db.insert(goods).values(dto);
-}
-//# sourceMappingURL=device.service.js.map
